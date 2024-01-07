0538d27e74d471170e271d9b4797829dadb1773e
Tommy van der Vorst <tommy@pixelspark.nl>
2024-01-07 11:01:58 +0100

    feat: allow user to specify file name to decode, folder ID and password

--- main.mjs
+++ main.mjs
@@ -4,12 +4,13 @@ import base32Encode from "base32-encode";
 import * as miscreant from "miscreant";
 import assert from "node:assert";
 
-const folderID = "tommy";
-const folderEncryptionPassword = "test";
+const folderID = process.env.ST_FOLDER || "tommy";
+const folderEncryptionPassword = process.env.ST_PASSWORD || "test";
+
 const fileNameToDecode =
-  "4.syncthing-enc/IS/DQJPKRK0GI2F23V1D4E32VQ8MQQNAN18RA1GU6SFEOAKB9VT93R8OALMM8";
-const fileNameToEncode = "wonnx/wonnx/Cargo.lock";
-const passwordTokenToDecode = "q+w5dDWKuvybKzTCQvRbgLrd2GNkaXvqW8NphqPJ";
+  process.argv.length > 2
+    ? process.argv[2]
+    : "4.syncthing-enc/IS/DQJPKRK0GI2F23V1D4E32VQ8MQQNAN18RA1GU6SFEOAKB9VT93R8OALMM8";
 
 const prefix = "syncthing";
 
@@ -24,10 +25,12 @@ scrypt(
       return null;
     }
     console.log("Key", key.toString("hex"));
-    assert(
-      key.toString("hex") ===
-        "ad69f63a34ea3244c5b326d2289568c55d61e9bc2e947fe83f39be2a8261756b"
-    );
+    if (folderID === "tommy" && folderEncryptionPassword === "test") {
+      assert(
+        key.toString("hex") ===
+          "ad69f63a34ea3244c5b326d2289568c55d61e9bc2e947fe83f39be2a8261756b"
+      );
+    }
 
     const aead = await miscreant.AEAD.importKey(
       key,
@@ -45,9 +48,24 @@ scrypt(
       Buffer.from(passwordToken).toString("base64")
     );
 
+    // Decode file name (A.syncthing-enc/AB/CDEF...)
+    const cleaned = fileNameToDecode.replace(/(\.syncthing-enc)|\//g, "");
+    const fnDecoded = Buffer.from(base32Decode(cleaned, "RFC4648-HEX"));
+    const res = await aead._siv.open(fnDecoded, [new Uint8Array(0)]);
+    const fileNameDecoded = Buffer.from(res).toString("utf8");
+    console.log("Decrypted file name:", fileNameDecoded);
+
+    // Decrypt password token (should result in plaintext of folderID + "syncthing")
+    const encrRes = await aead._siv.open(passwordToken, [new Uint8Array(0)]);
+    console.log(
+      "Decrypted password token:",
+      Buffer.from(encrRes).toString("utf8")
+    );
+    assert(Buffer.from(encrRes).toString("utf8") === prefix + folderID);
+
     // Encode a file name
     const encodedFileNameBytes = await aead._siv.seal(
-      Buffer.from(fileNameToEncode, "utf8"),
+      Buffer.from(fileNameDecoded, "utf8"),
       [new Uint8Array(0)]
     );
     const encodedFnBase32 = base32Encode(
@@ -62,21 +80,5 @@ scrypt(
       encodedFnBase32.substring(3); // TODO add more slashes when name gets too long
     console.log("Encoded filename: ", mangledFileName);
     assert(mangledFileName === fileNameToDecode);
-
-    // Decode file name (A.syncthing-enc/AB/CDEF...)
-    const cleaned = fileNameToDecode.replace(/(\.syncthing-enc)|\//g, "");
-    const fnDecoded = Buffer.from(base32Decode(cleaned, "RFC4648-HEX"));
-    const res = await aead._siv.open(fnDecoded, [new Uint8Array(0)]);
-    console.log("Decrypted file name:", Buffer.from(res).toString("utf8"));
-    assert(Buffer.from(res).toString("utf8") === fileNameToEncode);
-
-    // Decrypt password token (should result in plaintext of folderID + "syncthing")
-    const encrToken = Buffer.from(passwordTokenToDecode, "base64");
-    const encrRes = await aead._siv.open(encrToken, [new Uint8Array(0)]);
-    console.log(
-      "Decrypted password token:",
-      Buffer.from(encrRes).toString("utf8")
-    );
-    assert(Buffer.from(encrRes).toString("utf8") === prefix + folderID);
   }
 );
